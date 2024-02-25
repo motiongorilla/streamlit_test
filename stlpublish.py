ce2b9de0d6a2d0184de303735f32afcf91738069
motiongorilla <nize.afonya@gmail.com>
2024-02-25 18:51:36 +0000

    testing widgets and graphs

--- stlpublish.py
+++ stlpublish.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas
 
 data = {
-        'Series_1':[1,3,4,5,7,999],
-        'Series_2':[10,3,59,100,231,123],
+        'Series_1':[1,3,4,5,7,15],
+        'Series_2':[10,3,0,1,19,22],
         'Series_3':[1,5,6,2,25,6]
         }
 
@@ -14,4 +14,8 @@ st.subheader("Checking out streamlit workflow")
 st.write("Updated text just to see if the update is going to come through!")
 
 st.write(df)
+st.line_chart(df)
+st.area_chart(df)
 
+myslider = st.slider("Celsius")
+st.write(myslider, "In Fahrenheit is", myslider*9/5+32)
